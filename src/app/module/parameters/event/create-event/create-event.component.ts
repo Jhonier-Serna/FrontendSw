e4f8a49d7329ab6bdf65b203fc77506db8075fe3
Jhonier-Serna <jhoniier.serna@gmail.com>
2024-06-12 16:03:36 -0500

    Feature: Se agrega el formulario precargado para editar un evento

--- src/app/module/parameters/event/create-event/create-event.component.ts
+++ src/app/module/parameters/event/create-event/create-event.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
-import { forkJoin, Observable, of } from 'rxjs';
+import { Observable, forkJoin, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { ModalComponent } from '../../../../public/modal/modal.component';
 import {
   FormBuilder,
   FormGroup,
-  Validators,
   ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
 import {
   Storage,
@@ -35,7 +35,6 @@ interface SelectedFile {
 export class CreateEventComponent implements OnInit {
   fGroup: FormGroup = new FormGroup({});
   selectedFiles: SelectedFile[] = [];
-  uploadProgress$: Observable<number>[] = [];
 
   constructor(
     private fb: FormBuilder,
@@ -108,22 +107,11 @@ export class CreateEventComponent implements OnInit {
     this.selectedFiles.splice(index, 1);
   }
 
-  uploadFiles(): Observable<string[]> {
-    const uploadObservables = this.selectedFiles.map((selectedFile) =>
-      this.uploadFile(selectedFile.file).pipe(catchError(() => of('')))
-    );
-
-    return forkJoin(uploadObservables).pipe(
-      map((results) => results.filter((result) => !!result))
-    );
-  }
-
   async saveEvent() {
     if (this.fGroup.invalid) {
       console.log(this.fGroup);
       const dialogData = {
-        imageSrc:
-          'https://previews.dropbox.com/p/thumb/ACTBrDQQPxSBFvfNeSzeiRWh03o87-EmptEX9exoMrMW4Qbz-dCnC9R80DOp-IqmjbbKRwsNKNsTDar-wKrW0-cS25Akoe7FydN1b6n1Cn5HK9U0WyqP4eX0MKI9tWckTNMfLlpk8-1lS8NJgIJEgFx9pgX0BECK1CS-LTgKZsDdta3nyZwMEvhtV_r9qd9nBJajw9aJ1MTwqG8kZ8vaOc9BKFHaHu6RC9c5CXWkXexlqH5PHNstkUQe0kP7QSX5Iucc-XgycA49avU7vzqAtpxJLIZAGofLDUSuqboytVlOhUBskxfYjhbN7FeCSlOFrM1KHb9rdpLXTctVdsrhsq5b/p.png',
+        imageSrc: '...',
         message: 'Error al crear el evento!',
         message2: 'Reintentar',
       };
@@ -132,35 +120,19 @@ export class CreateEventComponent implements OnInit {
     }
 
     try {
-      this.uploadFiles().subscribe({
-        next: (downloadURLs) => {
-          const formData = { ...this.fGroup.value, fileLinks: downloadURLs };
-          console.log(formData);
-          const dialogData = {
-            imageSrc:
-              'https://uc923483daef064c1e5e5423d1b0.previews.dropboxusercontent.com/p/thumb/ACSa1kTByuPUsNWKJY0yc_9NxFIpwaaqQEsdurGsVhwW_D5dg66j1UciegR9NcdqA5SXZfiGT4G__p5zI1Yzv3c9ytvvSCJMTI-ydTmdQjbPMmXluHzmqQ9WFHOOvo7ycky-H68X9tyoVKb3cRBo7rQq1VCsLixiTIFLhJn_OW-Ubs3j3bbto8HLJ3qnY_mDCAyn2eywaRlZBC8Q1mGPDWPqhqb84iT1yUnV_QMIgVWCLNvLwOeuOfZSKx9ETbjuY5zkM5aI1CVe_RWT5fN1bhzf6i837Ho3jTN2fZPvdKB-JZbOiqx13a1TQr7GIkO2OviXTSJwgZyIl9qjmEHxKK2BicthhUQnhuCMh-7Adf7qFTyxL_i6KJNWfQGlOUl4Wj4_pG16lphPZ3h1YhXvU7SbI8OhBfEM9GvAB7TisV_xkavUx-gfKnB9OhLSW36cO6Q/p.png',
-            message: '¡Evento creado exitosamente!',
-            message2: 'Gracias!',
-          };
-          this.dialog.open(ModalComponent, { data: dialogData });
-          return;
-        },
-        error: (error) => {
-          console.error('Error uploading files: ', error);
-          const dialogData = {
-            imageSrc:
-              'https://previews.dropbox.com/p/thumb/ACTBrDQQPxSBFvfNeSzeiRWh03o87-EmptEX9exoMrMW4Qbz-dCnC9R80DOp-IqmjbbKRwsNKNsTDar-wKrW0-cS25Akoe7FydN1b6n1Cn5HK9U0WyqP4eX0MKI9tWckTNMfLlpk8-1lS8NJgIJEgFx9pgX0BECK1CS-LTgKZsDdta3nyZwMEvhtV_r9qd9nBJajw9aJ1MTwqG8kZ8vaOc9BKFHaHu6RC9c5CXWkXexlqH5PHNstkUQe0kP7QSX5Iucc-XgycA49avU7vzqAtpxJLIZAGofLDUSuqboytVlOhUBskxfYjhbN7FeCSlOFrM1KHb9rdpLXTctVdsrhsq5b/p.png',
-            message: 'Error al subir los archivos!',
-            message2: 'Reintentar',
-          };
-          this.dialog.open(ModalComponent, { data: dialogData });
-        },
-      });
+      const downloadURLs = await this.uploadFiles().toPromise();
+      const formData = { ...this.fGroup.value, fileLinks: downloadURLs };
+      console.log(formData);
+      const dialogData = {
+        imageSrc: '...',
+        message: '¡Evento creado exitosamente!',
+        message2: 'Gracias!',
+      };
+      this.dialog.open(ModalComponent, { data: dialogData });
     } catch (error) {
       console.error('Error uploading files: ', error);
       const dialogData = {
-        imageSrc:
-          'https://previews.dropbox.com/p/thumb/ACTBrDQQPxSBFvfNeSzeiRWh03o87-EmptEX9exoMrMW4Qbz-dCnC9R80DOp-IqmjbbKRwsNKNsTDar-wKrW0-cS25Akoe7FydN1b6n1Cn5HK9U0WyqP4eX0MKI9tWckTNMfLlpk8-1lS8NJgIJEgFx9pgX0BECK1CS-LTgKZsDdta3nyZwMEvhtV_r9qd9nBJajw9aJ1MTwqG8kZ8vaOc9BKFHaHu6RC9c5CXWkXexlqH5PHNstkUQe0kP7QSX5Iucc-XgycA49avU7vzqAtpxJLIZAGofLDUSuqboytVlOhUBskxfYjhbN7FeCSlOFrM1KHb9rdpLXTctVdsrhsq5b/p.png',
+        imageSrc: '...',
         message: 'Error al subir los archivos!',
         message2: 'Reintentar',
       };
@@ -170,5 +142,15 @@ export class CreateEventComponent implements OnInit {
 
   get getformGroup() {
     return this.fGroup.controls;
+  }
+
+  private uploadFiles(): Observable<string[]> {
+    const uploadObservables = this.selectedFiles.map((selectedFile) =>
+      this.uploadFile(selectedFile.file).pipe(catchError(() => of('')))
+    );
+
+    return forkJoin(uploadObservables).pipe(
+      map((results) => results.filter((result) => !!result))
+    );
   }
 }
--- src/app/module/parameters/event/edit-event/edit-event.component.ts
+++ src/app/module/parameters/event/edit-event/edit-event.component.ts
@@ -1,12 +1,211 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, formatDate } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { forkJoin, Observable, of, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { ModalComponent } from '../../../../public/modal/modal.component';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import {
+  Storage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+  UploadTaskSnapshot,
+} from '@angular/fire/storage';
+
+interface SelectedFile {
+  file: File;
+  preview: string;
+  type: string;
+  downloadURL?: string;
+}
 
 @Component({
   selector: 'app-edit-event',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './edit-event.component.html',
-  styleUrl: './edit-event.component.css'
+  styleUrls: ['./edit-event.component.css'],
 })
-export class EditEventComponent {
+export class EditEventComponent implements OnInit {
+  event = {
+    id: 1,
+    name: 'Andres Cepeda 1',
+    categoria: 'Concierto',
+    fecha: '2024-06-12',
+    plazas: 500,
+    HInicio: '17:00',
+    HFin: '20:00',
+    lugar: 'Plaza de toros',
+    encargado: 'Alcaldía',
+    descripcion:
+      '¡Andrés Cepeda llega a Manizales con su gira "Nuestra Vida en Canciones"!Prepárate para una noche inolvidable llena de romanticismo y nostalgia con uno de los artistas más reconocidos de Colombia. Andrés Cepeda se presentará en la Plaza de Toros de Manizales el próximo 29 de noviembre de 2024 a las 8:30 p.m.En este concierto, Cepeda repasará sus más grandes éxitos, como "Lo Que Faltaba", "Mil Tángos", "A Favor de Ti", "La Ruta Púrpura" y "Yo Me Enamoré", entre muchas otras. Además, contará con la participación de invitados especiales.No te pierdas la oportunidad de vivir una experiencia única junto a Andrés Cepeda y sus canciones.',
+    archivos: [
+      'https://firebasestorage.googleapis.com/v0/b/manizalesinteligente.appspot.com/o/uploads%2Fcepeda1.jpg?alt=media&token=1f1e99d6-995d-4221-8b13-1208ac776ad2',
+      'https://firebasestorage.googleapis.com/v0/b/manizalesinteligente.appspot.com/o/uploads%2Fcepeda2.jpg?alt=media&token=42a5d583-d1d2-40b0-a14b-84b1b932e5e4',
+      'https://firebasestorage.googleapis.com/v0/b/manizalesinteligente.appspot.com/o/uploads%2Fcepeda.mp4?alt=media&token=1bfb7bbe-e929-433c-bd1b-b485c77c8777',
+    ],
+  };
 
+  fGroup: FormGroup = new FormGroup({});
+  selectedFiles: SelectedFile[] = [];
+  uploadProgress$: Observable<number>[] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private dialog: MatDialog,
+    private storage: Storage
+  ) {}
+
+  ngOnInit() {
+    this.FormBuild();
+    this.loadEventData();
+  }
+
+  FormBuild() {
+    this.fGroup = this.fb.group({
+      eventName: ['', [Validators.required]],
+      category: ['', [Validators.required]],
+      date: ['', [Validators.required]],
+      places: ['', [Validators.required]],
+      startTime: ['', [Validators.required]],
+      endTime: ['', [Validators.required]],
+      place: ['', [Validators.required]],
+      entityInCharge: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+    });
+  }
+
+  loadEventData() {
+    this.fGroup.patchValue({
+      eventName: this.event.name,
+      category: this.event.categoria,
+      date: this.event.fecha,
+      places: this.event.plazas,
+      startTime: this.event.HInicio,
+      endTime: this.event.HFin,
+      place: this.event.lugar,
+      entityInCharge: this.event.encargado,
+      description: this.event.descripcion,
+    });
+  }
+
+  onFileSelected(event: any) {
+    if (event.target.files) {
+      const files: File[] = Array.from(event.target.files as FileList);
+      files.forEach((file: File) => {
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.selectedFiles.push({
+            file,
+            preview: e.target.result,
+            type: file.type,
+          });
+        };
+        reader.readAsDataURL(file);
+      });
+    }
+  }
+
+  uploadFile(file: File): Observable<string> {
+    const filePath = `uploads/${Date.now()}_${file.name}`;
+    const fileRef = ref(this.storage, filePath);
+    const uploadTask = uploadBytesResumable(fileRef, file);
+
+    return new Observable<string>((observer) => {
+      uploadTask.on(
+        'state_changed',
+        (snapshot: UploadTaskSnapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          // console.log('Upload is ' + progress + '% done');
+        },
+        (error) => {
+          observer.error(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            observer.next(downloadURL);
+            observer.complete();
+          });
+        }
+      );
+    });
+  }
+
+  deleteFile(index: number) {
+    this.selectedFiles.splice(index, 1);
+  }
+
+  uploadFiles(): Observable<string[]> {
+    const uploadObservables = this.selectedFiles.map((selectedFile) =>
+      this.uploadFile(selectedFile.file).pipe(catchError(() => of('')))
+    );
+
+    return forkJoin(uploadObservables).pipe(
+      map((results) => results.filter((result) => !!result))
+    );
+  }
+
+  async saveEvent() {
+    if (this.fGroup.invalid) {
+      this.showErrorDialog('Error al crear el evento!');
+      return;
+    }
+
+    try {
+      let formData;
+      if (this.selectedFiles.length !== 0) {
+        this.uploadFiles().subscribe({
+          next: (downloadURLs) => {
+            formData = {
+              ...this.fGroup.value,
+              fileLinks: downloadURLs.concat(this.event.archivos),
+            };
+            this.showSuccessDialog('¡Evento creado exitosamente!');
+          },
+        });
+      } else {
+        formData = {
+          ...this.fGroup.value,
+          fileLinks: this.event.archivos,
+        };
+        this.showSuccessDialog('¡Evento creado exitosamente!');
+        console.log(formData);
+        return;
+      }
+    } catch (error) {
+      console.error('Error uploading files: ', error);
+      this.showErrorDialog('Error al subir los archivos!');
+    }
+  }
+
+  showErrorDialog(message: string) {
+    const dialogData = {
+      imageSrc:
+        'https://ucb3975a9b09efbd37ce9fccaf1e.previews.dropboxusercontent.com/p/thumb/ACRJITIMMfFZypkn7tLz_uda0dmaMv_FbFMUXitkfLaaGQ4Ztf1TJ3Hs41VwpB26p5-8RqBzmw4xqTFQLdPPL8wJo3b9MgGxo3RB88jCVjsoS3eape16gKcXYrdFVp6tUEX7ZssF-zl0z1N-j8lFGaPthggVHXOA4IiXLWuiI8YwPspIW7CaH1Yi6totz7ahOHvkEXQQIk4JaewS6s21LTikqFVbnhsh5C1hXehac29y2RVgZ1SY4IGEcJ5iX8NlyuwZ4snwF2nkEApdIKi_0BIMs6MHGRKOlLisdvmLBlre-ii-d9O-UQjK8xofFKS0V58QQ2ITwdC4sZR6ZAahbLifB7TPywIvn4zJO7ItSUHATPUBLGN8SWK--iJzS3eZr04/p.png',
+      message: message,
+      message2: 'Reintentar',
+    };
+    this.dialog.open(ModalComponent, { data: dialogData });
+  }
+
+  showSuccessDialog(message: string) {
+    const dialogData = {
+      imageSrc:
+        'https://uc923483daef064c1e5e5423d1b0.previews.dropboxusercontent.com/p/thumb/ACSfwGQCITIrb_8c73GPh0LhrcQsmye1odFHjs7144Mz0u9jByuZvm2BGPVulitxeZGwWdxoMwNppkvl1cLcmhloWc7IkMH7MnicSyxdh_DOerh2gaRiPslYw2LHkQcqpnmVzR33blTC1yGRVquahiIjatEdX0g871FmyI5-wMvqG760jH0Mp1D_SsDH4DPDQdA_L7BybKWmhQb8Gaw2lf-xLdLKDMv6RZ9z1nwgTSPdVmrji1E53vekQwqdps5-Tl8AVJG4yFE4cZ8EIVWTNTCJTtmMMTVgfVX4dBmngX9n38DhEvyHMGQQt3GzOYa3PM4hgAYF01-ybDU2_noamMwtmk-7IH4MBym1G6Rcd6dl9JZ93qvz97Gk9uzVLuj9TSc/p.png',
+      message: message,
+      message2: 'Gracias!',
+    };
+    this.dialog.open(ModalComponent, { data: dialogData });
+  }
+
+  get getformGroup() {
+    return this.fGroup.controls;
+  }
 }
