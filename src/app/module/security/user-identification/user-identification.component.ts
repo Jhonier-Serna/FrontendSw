dbd6cc3454e7a1327cb667a5e1beea44ff3d4392
Jhonier-Serna <jhoniier.serna@gmail.com>
2024-06-13 22:48:29 -0500

    Fix:Routing in login

--- src/app/module/security/user-identification/user-identification.component.ts
+++ src/app/module/security/user-identification/user-identification.component.ts
@@ -1,56 +1,45 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import { Router } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user-identification',
   standalone: true,
-  imports: [
-
-    ReactiveFormsModule,
-    CommonModule
-
-  ],
+  imports: [ReactiveFormsModule, CommonModule, RouterModule],
   templateUrl: './user-identification.component.html',
-  styleUrl: './user-identification.component.css'
+  styleUrl: './user-identification.component.css',
 })
 export class UserIdentificationComponent {
-fGroup: FormGroup = new FormGroup({});
+  fGroup: FormGroup = new FormGroup({});
 
-constructor(
-  private fb: FormBuilder,
-  private router: Router
-
-){
-
-}
-ngOnInit(){
-  this.buildForm();
-   //this.router.navigate('/security/user-identification')
-}
-
-buildForm(){
-  this.fGroup= this.fb.group({
-
-    user : ['', [Validators.required, Validators.email]],
-    password : ['', [Validators.required ]]
-
-  });
-
-}
-
-userIdentification(){
-  if (this.fGroup.invalid){
-    alert("Datos incompletos")
-  }else{
-    alert('identificando....')
+  constructor(private fb: FormBuilder, private router: Router) {}
+  ngOnInit() {
+    this.buildForm();
+    //this.router.navigate('/security/user-identification')
   }
 
-}
+  buildForm() {
+    this.fGroup = this.fb.group({
+      user: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
 
-get getformGroup(){
-  return this.fGroup.controls;
-}
+  userIdentification() {
+    if (this.fGroup.invalid) {
+      alert('Datos incompletos');
+    } else {
+      alert('identificando....');
+    }
+  }
 
+  get getformGroup() {
+    return this.fGroup.controls;
+  }
 }
