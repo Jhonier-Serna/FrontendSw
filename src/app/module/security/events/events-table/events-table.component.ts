0e15b363c72579798c7143470a073a6fcfb97cd6
Jhonier-Serna <jhoniier.serna@gmail.com>
2024-06-13 21:40:02 -0500

    Fix:Routerlink in eventsTable for edit event

--- src/app/module/security/events/events-table/events-table.component.ts
+++ src/app/module/security/events/events-table/events-table.component.ts
@@ -1,20 +1,25 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { EventModel } from '../../../../models/event.model';
 import { ParametersService } from '../../../../services/parameters.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-events-table',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, RouterModule],
   templateUrl: './events-table.component.html',
-  styleUrl: './events-table.component.css',
+  styleUrls: ['./events-table.component.css'],
 })
-export class EventsTableComponent {
+export class EventsTableComponent implements OnInit {
   eventList: EventModel[] = [];
 
-  constructor(private parametersService: ParametersService) {}
+  constructor(
+    private parametersService: ParametersService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.parametersService.eventList().subscribe({
