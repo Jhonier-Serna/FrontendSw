2da13dda861a071fe50aa3c49b2a2beabe921bc6
Vanessa Osorio <vanessa.osorios@opitech.com.co>
2024-06-14 00:28:57 -0500

    enrutamiento con routerLink

--- src/app/module/security/forgot-password/forgot-password.component.ts
+++ src/app/module/security/forgot-password/forgot-password.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, RouterModule],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.css'
 })
 export class ForgotPasswordComponent {
+  fGroup: any;
+
+  constructor(private fb: FormBuilder, private router: Router) {}
+  ngOnInit() {
+    this.buildForm();
+    //this.router.navigate('/security/user-identification')
+  }
+
+
+  buildForm() {
+    this.fGroup = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+
+  }
+
+  get getformGroup() {
+    return this.fGroup.controls;
+  }
 
 }
--- src/app/public/master-page/side-menu/side-menu.component.ts
+++ src/app/public/master-page/side-menu/side-menu.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-side-menu',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, RouterModule],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css'
 })
 export class SideMenuComponent {
+  fGroup: any;
+
+  constructor(private fb: FormBuilder, private router: Router) {}
+  ngOnInit() {
+    this.buildForm();
+    //this.router.navigate('/security/user-identification')
+  }
+
+
+  buildForm() {
+    this.fGroup = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+
+  }
+
+  get getformGroup() {
+    return this.fGroup.controls;
+  }
 
 }
